38c73198c8e9536079e3c0bf94c95c2ef5098ebd
Ash Wilson <smashwilson@gmail.com>
2021-04-02 15:22:33 -0400

    Scaffolding for command structure

--- lib/branchtree.rb
+++ lib/branchtree.rb
@@ -1,6 +1,23 @@
 require "tty-command"
 
 module Branchtree
+  def self.execute(argv)
+    command_classes = {
+      "show" => Branchtree::Commands::Show
+    }
+
+    command_name = argv.shift || "show"
+    command_class = command_classes[command_name]
+    unless command_class
+      $stderr.puts "Unrecognized command: #{command_name}"
+      $stderr.puts "Available commands: #{command_classes.keys.join(", ")}"
+      exit 1
+    end
+    command = command_class.new
+    command.parse(argv)
+    command.execute
+  end
+
   module Context
     class << self
       attr_writer :cmd
@@ -19,3 +36,6 @@ end
 require "branchtree/version"
 require "branchtree/branch"
 require "branchtree/tree"
+require "branchtree/situation"
+require "branchtree/commands/common"
+require "branchtree/commands/show"
---
+++ lib/branchtree/commands/common.rb
@@ -0,0 +1,37 @@
+require "tty-option"
+
+module Branchtree
+  module Commands
+    class Common
+      include TTY::Option
+    
+      usage do
+        program "branchtree"
+      end
+    
+      flag :mapfile do
+        short "-m"
+        long "--mapfile PATH"
+        desc "Path to the YAML file describing desired branch topography"
+        default ENV.fetch("BRANCHTREE_MAPFILE", File.join(ENV["HOME"], "branchtree-map.yml"))
+      end
+
+      option :help do
+        short "-h"
+        long "--help"
+        desc "Display this message"
+      end
+
+      def execute
+        if params[:help]
+          puts help
+          exit 0
+        end
+      end
+
+      def load_tree
+        Tree.load(params[:mapfile])
+      end
+    end
+  end
+end
